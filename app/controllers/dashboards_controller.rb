80da78d1672ebd1e9a78c805aff8286c3e26f305
Marc Rodenbusch <marcrodenbusch@gmail.com>
2021-11-24 17:42:40 +0100

    finally

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,7 @@ class DashboardsController < ApplicationController
   def dashboard
     @events = current_user.events
     @bookings = Booking.where(event: @events)
-    @host_events = current_user.venues.first.events
+    @host_events = current_user.venues.first.events unless current_user.venues.empty?
     @host_bookings = Booking.where(event: @host_events)
   end
 end
