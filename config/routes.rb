db260b40031548e0cf77d32dfd0377337f9c925e
Damian Beissel <damian.beissel@googlemail.com>
2021-11-25 14:20:42 +0100

    showpage booking

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :venues, only: [ :index, :show ]
   resources :users, only: [ :index, :show ]
-  resources :bookings, only: [ :create ]
+  resources :bookings, only: [ :create, :show ]
 end
