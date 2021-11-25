e641ceb8f2af223b5125420d5d6c078c69da7830
Damian Beissel <damian.beissel@googlemail.com>
2021-11-25 17:07:24 +0100

    added websocket, live chat possible

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,18 @@
 class MessagesController < ApplicationController
+ 
   def create
     @booking = Booking.find(params[:booking_id])
     @message = Message.new(message_params)
     @message.booking = @booking
     @message.user = current_user
-    if @message.save
+    if @message.save!
+      BookingChannel.broadcast_to(
+        @booking,
+        render_to_string(partial: "message", locals: {message: @message})
+      )
       redirect_to booking_path(@booking)
     else
-      render "booking/show"
+      render "bookings/show"
     end
   end
 
