2b54662f0609a4714f0843656fc60f205b2dd20a
Damian Beissel <damian.beissel@googlemail.com>
2021-11-29 10:16:58 +0100

    generated model artist_profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_142441) do
+ActiveRecord::Schema.define(version: 2021_11_29_085655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artist_profils", force: :cascade do |t|
+    t.string "artistname"
+    t.string "genre"
+    t.string "instagram"
+    t.string "bandcamp"
+    t.string "soudcloud"
+    t.string "youtube"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_artist_profils_on_user_id"
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.integer "status"
@@ -74,6 +87,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_142441) do
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
+  add_foreign_key "artist_profils", "users"
   add_foreign_key "bookings", "events"
   add_foreign_key "bookings", "users"
   add_foreign_key "events", "venues"
