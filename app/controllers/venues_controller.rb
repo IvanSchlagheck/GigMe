4e0eff0971f380dda6c3841c4a0bc96eacf803c5
Marc Rodenbusch <marcrodenbusch@gmail.com>
2021-11-29 16:48:53 +0100

    advanced mapboxing

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -8,7 +8,8 @@ class VenuesController < ApplicationController
     @markers = @venues.geocoded.map do |venue|
       {
         lat: venue.latitude,
-        lng: venue.longitude
+        lng: venue.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { venue: venue })
       }
     end
   end
@@ -18,7 +19,7 @@ class VenuesController < ApplicationController
 
     start_date = params.fetch(:start_date, Date.today).to_date
     @events = Event.where(starts_at: start_date.beginning_of_week..start_date.end_of_week)
-    
+
   end
 
   def new
