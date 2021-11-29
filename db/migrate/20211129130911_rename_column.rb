94e6f0f56e7c93fc049492293ddde09c0edb48e1
ivan schlagheck <ivansch05@gmail.com>
2021-11-29 14:28:40 +0100

    changed events table

--- db/migrate/20211129130911_rename_column.rb
+++ db/migrate/20211129130911_rename_column.rb
@@ -1,4 +1,7 @@
 class RenameColumn < ActiveRecord::Migration[6.0]
   def change
+    change_table :events do |t|
+      t.rename :starts_at, :start_time
+      t.rename :ends_at, :end_time
   end
 end
