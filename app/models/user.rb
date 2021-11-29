1d3a79fce3a22ec33819e533c6b36186837faa03
Damian Beissel <damian.beissel@googlemail.com>
2021-11-29 10:30:42 +0100

    realted tables in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :venues
   has_many :bookings
   has_many :events, through: :bookings
+  has_one :artist_profil
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
