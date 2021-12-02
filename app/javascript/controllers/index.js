a48eb156c596ab8bf872f5975336aeb5d4e8c71a
Marc Rodenbusch <marcrodenbusch@gmail.com>
2021-12-02 13:00:52 +0100

    implemented flatpickr to create events in a better way

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,21 @@
-// Load all the controllers within this directory and all subdirectories. 
+// Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
+import './flatpickr';
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+
+// $(funtion() {
+
+//   $('form').submit(function(event){
+//     let date = JSON.stringify($(this).serializeArray());
+//     $('#message').text(date);
+//     event.preventDefault();
+//   })
+
+// });
